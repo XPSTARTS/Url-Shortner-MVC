9ace7a4ce781a34ef18b9199376bca8c4e649b28
XPSTARTS <abdulmoid47628@gmail.com>
2026-08-04 01:27:29 +0500

    Add debug endpoint

--- src/UrlShortner.Web/Controllers/HomeController.cs
+++ src/UrlShortner.Web/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace UrlShortner.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger , IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
@@ -38,5 +40,25 @@ public class HomeController : Controller
         }
 
         return View("Error");
+    }
+
+    [HttpGet("/debug")]
+    public IActionResult Debug()
+    {
+        var redis = Environment.GetEnvironmentVariable("REDIS_CONNECTION") ?? "NOT SET";
+        var db = Environment.GetEnvironmentVariable("DB_CONNECTION") ?? "NOT SET";
+        var jwt = Environment.GetEnvironmentVariable("JWT_SECRET_KEY") ?? "NOT SET";
+
+        var configRedis = _configuration.GetConnectionString("Redis") ?? "NOT SET";
+        var configDb = _configuration.GetConnectionString("DefaultConnection") ?? "NOT SET";
+
+        return Ok(new
+        {
+            env_redis = redis,
+            env_db = db,
+            env_jwt = jwt,
+            config_redis = configRedis,
+            config_db = configDb
+        });
     }
 }
